7e7d644bef73bfc6c400fd9fc5b9f0be4d08a3de
sumit.vishvakarma <svishvakarma@bestpeers.com>
2022-08-15 22:01:24 +0530

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
-
+  
   def after_sign_in_path_for(a)
     if a.seller?
       sellers_path
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end 
   end
-
+ 
   protected
 
   def update_allowed_parameters
--- app/controllers/lineitems_controller.rb
+++ app/controllers/lineitems_controller.rb
@@ -1,8 +1,18 @@
 class LineitemsController < ApplicationController
     before_action :authenticate_user!
       def index
-         @lineitems = current_user.lineitems
+          @order=current_user.orders.last
+          if @order
+            if @order.cart?
+              @lineitems = @order.lineitems
+            else
+              @lineitem=nil
+            end
+          else
+             @lineitem=nil
+          end 
       end
+
       def create
         @user=User.find(current_user.id)
         @order=@user.orders.last
@@ -42,7 +52,43 @@ class LineitemsController < ApplicationController
                     redirect_to buyers_path
                 end 
             else 
-               @order = @user.orders.new()
+               sample
+            end
+        else
+          sample
+        end
+      
+       
+      end
+      def destroy
+        @lineitem = Lineitem.find(params[:id])
+        @lineitem.destroy
+        redirect_to lineitems_path
+      end
+      def payment
+        @user=User.find(current_user.id)
+      end
+
+    def payment_type
+      @user=User.find(current_user.id)
+      @order=@user.orders.last
+      @order.update(payment_para)
+      @order.shipped!
+      if @order.save
+        flash[:message] = "Your Order is placed"
+          redirect_to buyers_path
+      end
+    end
+      
+      private
+      def carts_params
+       params.permit(:price,:quantity,:product_id)
+      end
+      def payment_para
+        params.permit(:payment_mode)
+      end
+      def sample
+        @order = @user.orders.new()
                if @order.save
                  @order.cart!
                  @lineitem = @user.orders.last.lineitems.new(carts_params)
@@ -56,51 +102,6 @@ class LineitemsController < ApplicationController
                     redirect_to buyers_path
                 end
                end
-            end
-        else
-            @order = @user.orders.new()
-            if @order.save
-                @order.cart!
-                @lineitem = @user.orders.last.lineitems.new(carts_params)
-                @lineitem.user_id = current_user.id
-                
-                if @lineitem.save
-                   
-                    @order = @user.orders.last
-
-                    @order.total_price = @order.total_price + (@lineitem.price*@lineitem.quantity)
-                    @order.total_quantity = @order.total_quantity + @lineitem.quantity
-                    @order.save
-                    redirect_to buyers_path
-                end
-            end
-        end
-       
-      end
-      def destroy
-        @lineitem = Lineitem.find(params[:id])
-        @lineitem.destroy
-        redirect_to lineitems_path
-      end
-      def payment
-      end
-
-    def payment_type
-      @user=User.find(current_user.id)
-      @order=@user.orders.first.lineitems
-      @order.update(payment_para)
-      if @order.save
-          redirect_to buyers_path
-      end
-    end
-      
-      private
-      def carts_params
-       params.permit(:price,:quantity,:product_id)
-      end
-      def payment_para
-        params.permit(:payment_mode)
-      end
-      
+      end   
 end
   
